db2093d7a7cddd021b134faf432db248d24ef46b
Do phuc Hung <hsdev.29032004@gmail.com>
2026-03-28 10:02:33 +0700

    polish: remove redundant code

--- src/modules/system/system.service.ts
+++ src/modules/system/system.service.ts
@@ -5,7 +5,7 @@ import { PrismaService } from '../../infras/prisma/prisma.service';
 
 @Injectable()
 export class SystemService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async getBanks() {
     return this.prisma.bank.findMany({
@@ -56,21 +56,5 @@ export class SystemService {
       where: { id },
       data: { isDeleted: true },
     });
-  }
-
-  findAll() {
-    return `This action returns all system`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} system`;
-  }
-
-  update(id: number, updateSystemDto: UpdateSystemDto) {
-    return `This action updates a #${id} system`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} system`;
   }
 }
